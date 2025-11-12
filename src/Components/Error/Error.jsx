064381c4eb907f1610edb117c866a117fee70142
SadiaHaque05 <sadiahaque5403@gmail.com>
2025-11-12 19:00:18 +0600

    added the error page

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router";
+import errorImg from "../../assets/girl error.jpg"
 
 const Error = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <h2 className="text-2xl mb-6">Oops! Page Not Found</h2>
+      <img className="h-[450px] w-[450px] rounded-4xl" src={errorImg} alt="" />
+      <h2 className="text-3xl mt-3 mb-6">Oops! Page Not Found</h2>
       <p className="mb-6 text-white/70">
         The page you are looking for does not exist or has been removed.
       </p>
